bb8db2275ce8c930f8697f1fa221fa106d47513d
Miles McLeod <milesmcleod123@gmail.com>
2017-12-20 19:14:16 -0800

    Complete timeout math

--- app/util/math.js
+++ app/util/math.js
@@ -1,15 +1,36 @@
 class Math {
 
   constructor(options) {
+    this.calculateMoments = this.calculateMoments.bind(this);
+    this.notify = this.notify.bind(this);
+    this.generateTimeouts = this.generateTimeouts.bind(this);
+
     this.frequency = options.frequency;
     this.startTime = options.startTime; // milleseconds since midnight
     this.endTime = options.endTime; // milliseconds since midnight
+
     this.moments = [];
     this.timeoutIds = [];
+    this.midnightID = undefined;
+
     const date = new Date();
     const offset = date.getTimezoneOffset();
     this.currentDate = new Date(Date.now() - (offset * 60 * 1000)); //date
     this.currentMoment = this.currentDate.getTime(); // milliseconds
+  }
+
+  notify() {
+
+  }
+
+  generateTimeouts() {
+    for (let i = 0; i < this.moments.length; i++) {
+      const moment = this.moments.pop;
+      const id = setTimeout(() => {
+        this.notify();
+      }, moment);
+      this.timeoutIds.push(id);
+    }
   }
 
   calculateMoments() {
@@ -22,12 +43,13 @@ class Math {
     }
   }
 
-  generateNotification() {
-
-  }
-
-  generateNotificationTimeouts() {
-
+  setMidnightInterval() {
+    const msPerDay = (24 * 60 * 60 * 1000);
+    const msTilMidnight = msPerDay - (this.currentMoment % msPerDay);
+    this.midnightID = setTimeout(() => {
+      this.calculateMoments();
+      this.generateTimeouts();
+    }, msTilMidnight);
   }
 
 }
